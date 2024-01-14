431f434274b7952847cf50d4a41a0ca7e5ec3496
sangwani <mightypz2@gmail.com>
2024-01-14 12:31:21 +0200

    add lipila logo & favicon

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -3,38 +3,44 @@ import { Outlet, Link } from "react-router-dom";
 const Nav = () => {
     return (
         <>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-                <div className="container-fluid">
-                    <Link className="navbar-brand" to="/">
-                        Lipila
-                    </Link>
-                    <button
-                        className="navbar-toggler"
-                        type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#navbarSupportedContent"
-                        aria-controls="navbarSupportedContent"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation"
-                    >
-                        <span className="navbar-toggler-icon" />
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/">Send Money</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/about"> About us</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/contact">
-                                    Contact us
-                                </Link>
-                            </li>
-                        </ul>
-
-                    </div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a className="navbar-brand" href="/">
+                    <img
+                        src="/logo.png"
+                        width={100}
+                        height={20}
+                        alt="Lipila logo"
+                    />
+                </a>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon" />
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link" href="/">
+                               Send Money
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/about">
+                                About
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/contact">
+                                Contact us
+                            </a>
+                        </li>
+                    </ul>
                 </div>
             </nav>
             <Outlet />
