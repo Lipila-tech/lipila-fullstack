1a65c1b5eb801dbc99f819ef7fc439ed733a4ac9
sangwani <mightypz2@gmail.com>
2024-01-16 22:00:59 +0200

    use HashRouter

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
-import { HashRouter } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import * as ReactDOM from "react-dom";
 import Nav from './components/layout/Nav';
-import LandingPage from './components/pages/info/LandingPage';
 import AboutPage from './components/pages/info/AboutPage';
 import ContactPage from './components/pages/info/ContactPage';
 import PaymentInfoPage from './components/pages/info/PaymentInfoPage';
 import ParentComponent from './components/pages/ParentComponent';
 import PaymentButton from './components/pages/PaymentButton';
-import Popup from './components/pages/PopUpComponent';
 import PersonList from './components/TestAxios';
 
 function App() {
   return (
     <HashRouter base="/">
       {/* <Routes>
-        <Route path="/" element={<Nav />}>
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/" element={<ParentComponent />} />
-          <Route path="/lipila" element={<PersonList/>}></Route>
+        <Route path="/" component={Nav}>
+          <Route path="/about" component={AboutPage} />
+          <Route path="/contact" component={ContactPage} />
+          <Route path="/" component={ParentComponent} />
+          <Route path="/lipila" component={PersonList}></Route>
         </Route>
       </Routes> */}
-      <AboutPage/>
-      <ParentComponent/>
+      {/* <Routes>
+        <Route path="/" Component={Nav}>
+        <Route path="/about" component={AboutPage} />
+        </Route>
+      </Routes> */}
+      <ParentComponent />
+      {/* <AboutPage/>
       <ContactPage/>
-      <PersonList/>
+      <PersonList/> */}
     </HashRouter>
   );
 }
