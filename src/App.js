b060fc103da9c5c6d399797573a668426ffcdd36
sangwani <mightypz2@gmail.com>
2024-01-22 19:29:37 +0200

    implement local state token authentication

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import React from "react";
 import Layout from "./components/Layout/Layout";
 import HomePage from "./components/HomePage";
-import LoginForm from "./components/Auth/Login";
+import Login from "./components/Auth/Login";
 import RegistrationForm from "./components/Auth/Register";
 
 import AboutPage from "./components/About/AboutPage"
@@ -15,22 +15,26 @@ import Preferences from "./components/Preferences/Preferences";
 
 
 function App() {
+  const [token, setToken] = React.useState();
+  if (!token) {
+    return <Login setToken={setToken} />
+  }
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Layout/>}>
-        <Route index element={<HomePage/>} />
-        <Route path="/send-money" element={<ParentComponent/>} />
-        <Route path="/about" element={<AboutPage/>} />
-        <Route path="/contact" element={<ContactPage/>} />
-        <Route path="/login" element={<LoginForm/>} />
-        <Route path="/register" element={<RegistrationForm/>} />
-        <Route path="/products" element={<Products/>} />
-        <Route path="/dashboard" element={<Dashboard/>} />
-        <Route path="/preferences" element={<Preferences/>} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/send-money" element={<ParentComponent />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<RegistrationForm />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/preferences" element={<Preferences />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 export default App;
--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,63 +1,83 @@
 import React from 'react';
 import { Form, Input, Button } from 'antd';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-const LoginForm = () => {
+
+async function loginUser(credentials) {
+    return fetch('http://localhost:8000/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credentials)
+    })
+        .then(data => data.json())
+}
+
+
+const Login = ({ setToken }) => {
     const [email, setEmail] = React.useState('');
     const [password, setPassword] = React.useState('');
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-
-        const data = {
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const token = await loginUser({
             email,
-            password,
-        };
-        // Send data to your server or perform other actions here
-    };
+            password
+        });
+        setToken(token);
+    }
 
     return (
         <div className="container p-3">
             <div className='card px-4'>
                 <p className='h8 py-3'>Login</p>
                 <div className='row gx-3'>
-                    <div className='col-12'>
-                        <div className="d-flex flex-column">
-                            <label htmlFor="email">Email</label>
-                            <input
-                                type="email"
-                                className="form-control"
-                                id="email"
-                                placeholder="Ex: example@email.com"
-                                onChange={(event) => setEmail(event.target.value)}
-                                required
-                            />
+                    <form onSubmit={handleSubmit}>
+                        <div className='col-12'>
+                            <div className="d-flex flex-column">
+
+                                <label htmlFor="email">Email</label>
+                                <input
+                                    type="email"
+                                    className="form-control"
+                                    id="email"
+                                    placeholder="Ex: example@email.com"
+                                    onChange={(event) => setEmail(event.target.value)}
+                                    required
+                                />
+                            </div>
                         </div>
-                    </div>
-                    <div className='col-12'>
-                        <div className="d-flex flex-column">
-                            <label htmlFor="passwor">Password</label>
-                            <input
-                                type='password'
-                                className="form-control"
-                                id="password"
-                                rows="5"
-                                placeholder="************"
-                                s onChange={(event) => setPassword(event.target.value)}
-                                required
-                            />
+                        <div className='col-12'>
+                            <div className="d-flex flex-column">
+                                <label htmlFor="passwor">Password</label>
+                                <input
+                                    type='password'
+                                    className="form-control"
+                                    id="password"
+                                    rows="5"
+                                    placeholder="************"
+                                    s onChange={(event) => setPassword(event.target.value)}
+                                    required
+                                />
+                            </div>
                         </div>
-                    </div>
-                </div><br />
-                <div>
-                    <div className='col-12'>
-                        <button type="submit" className="btn btn-primary mb-2">Login</button>
-                    </div>
+                        <div>
+                            <div className='col-12'>
+                                <button type="submit" className="btn btn-primary mb-2">Login</button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-                <Link to="/register">Register</Link>
+                <a href="/register">Register</a>
             </div>
-        </div>
+        </div >
     );
 };
 
-export default LoginForm;
+Login.propTypes = {
+    setToken: PropTypes.func.isRequired
+}
+
+export default Login;
